82e75878c69120cc45122a76d2bb9d6c09cf6aa6
Thegame2022 <Thegame2022@yandex.ru>
2024-12-06 18:10:09 +0300

    сумма от 1 до 60

--- PZ_5/PZ_5-13(number1).py
+++ PZ_5/PZ_5-13(number1).py
@@ -1,21 +1,20 @@
 #Найти сумму чисел ряда 1,2,3,...,60 с использованием функции нахождения суммы.
 #Использовать локальные переменные.
-def sum_of_series(n):
-    # Проверка на корректность входного параметра
-    if not isinstance(n, int) or n < 1:
+def sum_of_series(sumber):
+    #Проверка на рпавильность введенных данных
+    if not isinstance(sumber, int) or sumber < 1:
         raise ValueError("Входное значение должно быть целым числом больше или равным 1.")
-    # Локальная переменная для хранения суммы
+    #Локальная переменная для хранения суммы
     total_sum = 0
-    # Цикл для суммирования чисел от 1 до n
-    for i in range(1, n + 1):
+    #Цикл для суммирования чисел от 1 до n
+    for i in range(1, sumber + 1):
         total_sum += i
     return total_sum
-# Основная часть программы
 try:
-    # Установка значения n
-    n = 60
-    # Вызов функции для нахождения суммы чисел от 1 до n
-    result = sum_of_series(n)
-    print("Сумма чисел от 1 до", n, ":", result)
+    #Задаем sumber
+    sumber = 60
+    #Вызывае функцию для нахождения суммы чисел от 1 до sumber
+    result = sum_of_series(sumber)
+    print("Сумма чисел от 1 до", sumber, ":", result)
 except ValueError as e:
     print("Ошибка:", e)
