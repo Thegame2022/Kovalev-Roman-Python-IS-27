120566678e83f7c913766d26696683ddf7567044
Thegame2022 <Thegame2022@yandex.ru>
2024-12-14 01:03:26 +0300

    PZ-6 Number 2

--- PZ_6/PZ_6_13(number2).py
+++ PZ_6/PZ_6_13(number2).py
@@ -5,40 +5,47 @@
 def couple(R, A):
     if len(A) < 2:
         raise ValueError("Список должен содержать хотя бы два элемента")
-
     closest_pair = (A[0], A[1])  #Инициализируем начальную пару
     min_difference = abs((A[0] + A[1]) - R)
-
     for i in range(len(A) - 1):
         current_sum = A[i] + A[i + 1]
         difference = abs(current_sum - R)
-
         if difference < min_difference:
             min_difference = difference
             closest_pair = (A[i], A[i + 1])
-
     return sorted(closest_pair)
 
 if __name__ == "__main__":
     try:
         #Запрашиваем у пользователя ввод числа R
         R = float(input("Введите число R: "))
-
-        #Запрашиваем у пользователя ввод списка A
-        A = list(map(int, input("Введите элементы списка A через пробел: ").split()))
-
+        #Запрашиваем ввод элементов для списка A
+        B = input("Введите элементы списка A: ")
+        #Если цифры введены без пробелов,то обрабатываются как отдельные цифры
+        if ' ' not in B:
+            A = []
+            for char in B:
+                A.append(int(char))
+        else:
+            A = list(map(int, B.split()))
         #Выводим исходный список
         print(f"Исходный список: {A}")
-
         #Вызываем функцию поиска ближайшей пары
         closest_pair = couple(R, A)
-
         #Выводим ближайшую пару
         print(f"Ближайшая пара к {R}: {closest_pair}")
     except ValueError as e:
         print(f"Произошла ошибка: {e}")
     except Exception as e:
         print(f"Произошла непредвиденная ошибка: {e}")
+
+
+
+
+
+
+
+
 
 
 
