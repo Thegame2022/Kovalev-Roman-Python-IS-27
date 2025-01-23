312f04f43e9ee9aa8885369a808cd0b9fbab1ddc
Thegame2022 <Thegame2022@yandex.ru>
2025-01-23 22:50:50 +0300

    ПЗ-9

---
+++ PZ_9/PZ_9.py
@@ -0,0 +1,22 @@
+#Организовать словарь 10 русско-английских слов, обеспечивающий "перевод" русского слова на английского.
+ruan = {
+    "привет": "hello",
+    "дом": "house",
+    "дерево": "tree",
+    "собака": "dog",
+    "кошка": "cat",
+    "книга": "book",
+    "машина": "car",
+    "компьютер": "computer",
+    "человек": "human",
+    "город": "city"
+}
+while True:
+    enter = input("Введите русское слово для перевода: ").strip().lower()
+    if user_input == 'e':
+        break
+    if enter in ruan:
+        translate = ruan[enter]
+        print(f"Перевод слова {enter} - {translate}")
+    else:
+        print(f"Слово {enter} не найдено")
