ca158a6a1604a6fed9e0078e54083723ec79d398
Thegame2022 <Thegame2022@yandex.ru>
2025-04-24 23:39:28 +0300

    PZ-11_13-number-1

--- PZ_11/PZ_11_13-1.py
+++ PZ_11/PZ_11_13-1.py
@@ -8,10 +8,10 @@
 #Произведение элементов средней трети:
 import math
 #Создание исходного файла с последовательными положительными и отрицательными числами
-with open('input.txt', 'w') as file:
+with open('input(number1).txt', 'w') as file:
     file.write("1 -1 2 -2 3 -3 4 -4 5 -5 6 -6")
 #Чтение данных из файла с его обработкой
-with open('input.txt', 'r') as file:
+with open('input(number1).txt', 'r') as file:
     numbers = list(map(int, file.read().split()))
 #Количество элементов
 count = len(numbers)
@@ -22,7 +22,7 @@ third = len(numbers) // 3
 middle_third = numbers[third:2*third]
 product = math.prod(middle_third)
 #Запись результатов в новый файл
-with open('output.txt', 'w') as file:
+with open('output(number1).txt', 'w') as file:
     file.write("Исходные данные: " + ' '.join(map(str, numbers)) + "\n")
     file.write("Количество элементов: " + str(count) + "\n")
     file.write("Индекс первого максимального элемента: " + str(max_index) + "\n")
